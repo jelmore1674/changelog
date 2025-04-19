217ecdcf9adf91a7fac940539356805659bc95fa
Justin Elmore <jelmore1674@pm.me>
2025-04-19 00:25:33 -0500

    update jsdoc and add tests for isUnreleasedVersion

---
+++ src/parser/isUnreleasedVersion.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { isUnreleasedVersion } from "./isUnreleasedVersion";
+
+test.each([
+  { input: "Unreleased", expected: true },
+  { input: "1.0.0", expected: false },
+  { input: "0.1.1", expected: false },
+  { input: "uNRELEASED", expected: true },
+  { input: "unreleased", expected: true },
+  { input: "UnReleased", expected: true },
+])("isUnreleasedVersion($input) -> $expected", ({ input, expected }) => {
+  expect(isUnreleasedVersion(input)).toBe(expected);
+});
--- src/parser/isUnreleasedVersion.ts
+++ src/parser/isUnreleasedVersion.ts
@@ -2,6 +2,12 @@
  * Checks if version is `Unreleased`
  *
  * @param version - the version to check if unreleased
+ * @returns - if the current version is unreleased.
+ *
+ * @example
+ * ```js
+ * const isUnreleased = isUnreleasedVersion(version)
+ * ```
  */
 function isUnreleasedVersion(version: string) {
   return version.toLowerCase() === "unreleased";
