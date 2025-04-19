a395f797bbefb55fd210f6934fbe5cba879c8029
Justin Elmore <jelmore1674@pm.me>
2025-04-19 00:26:07 -0500

    add options for changelog style.

--- src/write/writeChangelog.test.ts
+++ src/write/writeChangelog.test.ts
@@ -76,4 +76,99 @@ _Initial Release_
 [3]: https://test.com
 `);
   });
+
+  test("Updates using the common-changelog heading.", () => {
+    const changes: Changelog = {
+      versions: [{
+        version: "Unreleased",
+        release_date: "TBD",
+        added: ["This change here."],
+      }],
+      links: [],
+    };
+
+    const changelog = writeChangelog(changes, { changelogStyle: "common-changelog" });
+
+    expect(changelog).toBe(`# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Common Changelog](https://common-changelog.org),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased] - TBD
+
+### Added
+
+- This change here.
+
+`);
+  });
+
+  test("Can handle dynamic headings.", () => {
+    const changes: Changelog = {
+      versions: [{
+        version: "Unreleased",
+        release_date: "TBD",
+        test: ["This change here."],
+        testing: ["This change here."],
+      }],
+      links: [],
+    };
+
+    const changelog = writeChangelog(changes, { dynamic: true });
+
+    expect(changelog).toBe(`# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased] - TBD
+
+### Test
+
+- This change here.
+
+### Testing
+
+- This change here.
+
+`);
+  });
+
+  test("Can handle custom heading.", () => {
+    const changes: Changelog = {
+      versions: [{
+        version: "Unreleased",
+        release_date: "TBD",
+        test: ["This change here."],
+        testing: ["This change here."],
+      }],
+      links: [],
+    };
+
+    const changelog = writeChangelog(changes, {
+      dynamic: true,
+      changelogStyle: "custom",
+      customHeading: "This is a custom heading.",
+    });
+
+    expect(changelog).toBe(`# Changelog
+
+This is a custom heading.
+
+## [Unreleased] - TBD
+
+### Test
+
+- This change here.
+
+### Testing
+
+- This change here.
+
+`);
+  });
 });
