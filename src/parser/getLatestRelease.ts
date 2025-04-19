afbe6ae02bd1ad8b80d8a0f09a5da8e1c1f3fd61
Justin Elmore <jelmore1674@pm.me>
2025-04-19 00:25:06 -0500

    update jsdoc and add test to getLatestRelease

--- src/parser/getLatestRelease.ts
+++ src/parser/getLatestRelease.ts
@@ -5,9 +5,16 @@ import { parseChangelog } from "./parseChangelog";
  * Get the latest release
  *
  * @param changelog - The changelog to get the latest release from.
+ * @returns the latest release from your changelog.
+ *
+ * @example
+ * ```js
+ * const changelog = readFileSync("CHANGELOG.md","utf8");
+ * const latestRelease = getLatestRelease(changelog);
+ * ```
  */
 function getLatestRelease(changelog: string) {
-  const versions = parseChangelog(changelog).versions.map(({ version }) => version).filter(v => v);
+  const versions = parseChangelog(changelog).versions.map(({ version }) => version).filter(Boolean);
   return latestSemver(versions);
 }
 
